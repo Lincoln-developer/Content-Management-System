807d95f41fbd546c9f609792647a55a735bb46b5
Lincoln-developer <angufibolinc@gmail.com>
2022-08-11 15:48:06 +0300

    added nodemailer and mail model

---
+++ Backend/Models/Mail-Options.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const mailOptionsSchema = new mongoose.Schema({
+  sender: {
+    type: String,
+    required: true,
+  },
+  receiver: {
+    type: String,
+    required: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+  body:{
+    type:String,
+    required:true
+  }
+});
+
+const Mail = mongoose.model('Mail',mailOptionsSchema);
+
+export default Mail;
--- Backend/Server.js
+++ Backend/Server.js
@@ -8,7 +8,32 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-const PORT = process.env.PORT;
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+        user:process.env.EMAIL,
+        pass:process.env.PASSWORD
+    }
+});
+
+const mailOptions = {
+    from :'flip@gmail.com',
+    to:'fly@gmail.com',
+    subject:'Automatic Email System',
+    text:'auto-email system is going down'
+};
+
+transporter.sendMail(mailOptions, (err, data)=> {
+    if(err){
+        console.log('Error occured in sending email',err)
+    }else{
+        console.log('Email is sent')
+    }
+})
+
+const PORT = 3300;
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
