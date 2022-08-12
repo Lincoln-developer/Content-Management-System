9cdd93bb07d044a185a8a374251232a20779a2e1
Lincoln-developer <angufibolinc@gmail.com>
2022-08-12 13:36:59 +0300

    added admin user model

---
+++ Backend/Models/admin-user-model.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const adminSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { collection: 'RegisteredUsers' }
+);
+
+const Admin = mongoose.model('Admin', adminSchema);
+
+export default Admin;
